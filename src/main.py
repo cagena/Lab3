c49beb9545821faa92d0b8cb501f0409df176dcd
cagena <98061439+cagena@users.noreply.github.com>
2022-02-08 16:15:15 -0800

    Lab 3 2/8 4:14pm

--- src/main.py
+++ src/main.py
@@ -19,48 +19,37 @@ import motor_agena_chiu
 import controller_agena_chiu
 import utime
 import print_task
-# def task1_fun ():
-#     """!
-#     Task which puts things into a share and a queue.
-#     """
-#     counter = 0
-#     while True:
-#         share0.put (counter)
-#         q0.put (counter)
-#         counter += 1
-
-#         yield (0)
-
 
 def task_motor1():
+    difference = 0
     start = utime.ticks_ms()
     while True:
         ## A variable that creates a timer which marks the current time.
         current = utime.ticks_ms()
-        difference = current - start
+        difference = (current - start)
         ## A variable that defines duty cycle for the controller's run function.
         duty_cycle = controller.run(encoder_drv1.read())
         motor_drv1.set_duty_cycle(duty_cycle)
-#         time1.put(difference)
-#         enc_pos1.put(encoder_drv1.read())
-        print_task.put('{:},{:}\r\n'.format(difference,encoder_drv1.read()))
-        if difference >= 1500:
-            break
+
+        if difference <= 1500:
+            print_task.put('{:},{:}\r\n'.format(difference,encoder_drv1.read()))
+            
         yield()
 
-def task2_fun ():
-    """!
-    Task which takes things out of a queue and share to display.
-    """
+def task_motor2():
+    difference = 0
+    start = utime.ticks_ms()
     while True:
-        # Show everything currently in the queue and the value in the share
-        print ("Share: {:}, Queue: ".format (share0.get ()), end='');
-        while q0.any ():
-            print ("{:} ".format (q0.get ()), end='')
-        print ('')
-
-        yield (0)
-
+        ## A variable that creates a timer which marks the current time.
+        current = utime.ticks_ms()
+        difference = (current - start)
+        ## A variable that defines duty cycle for the controller's run function.
+        duty_cycle = controller.run(encoder_drv2.read())
+        motor_drv2.set_duty_cycle(duty_cycle)
+        # The print portion is commented out for the second motor.
+#         if difference <= 1500:
+#             print_task.put('{:},{:}\r\n'.format(difference,encoder_drv2.read()))
+        yield()
 
 # This code creates a share, a queue, and two tasks, then starts the tasks. The
 # tasks run until somebody presses ENTER, at which time the scheduler stops and
@@ -73,47 +62,15 @@ if __name__ == "__main__":
     ## A variable that creates a motor driver for motor 1.
     motor_drv1 = motor_agena_chiu.MotorDriver(pyb.Pin.cpu.A10, pyb.Pin.cpu.B4, pyb.Pin.cpu.B5, 3)
     # A variable that creates a motor driver for motor 2.
-    # motor_drv2 = motor_agena_chiu.MotorDriver(pyb.Pin.cpu.C1, pyb.Pin.cpu.A0, pyb.Pin.cpu.A1, 5)
+    #motor_drv2 = motor_agena_chiu.MotorDriver(pyb.Pin.cpu.C1, pyb.Pin.cpu.A0, pyb.Pin.cpu.A1, 5)
     ## A variable that creates a controller driver.
     controller = controller_agena_chiu.ControllerDriver(0, 0)
     # Enable both motors.
     motor_drv1.enable()
-    # motor_drv2.enable()
-    # Set the duty cycle for both motors.
-    
-    ## A variable that requests for proportional gain from the user.
-#     x1 = input('Input Kp to run step response, input s to stop: ')
-#     try:
-#         float(x1)
-#     except:
-#         if x == 's':
-#             motor_drv1.set_duty_cycle(0)
-#     else:
-#         ## A variable that requests for set point from the user.
-#         y1 = input('Input set point: ')
-#         controller.set_gain(x1)
-#         controller.set_setpoint(y1)
-#         encoder_drv1.zero()
-    controller.set_gain(0.1)
-    controller.set_setpoint(16384)
-    encoder_drv1.zero()
-            
-#     ## A variable that requests for proportional gain from the user.
-#     Kp_motor1 = task_share.Share ('h', thread_protect = False, name = "Kp1 Share")
-#     x1 = input('Input Kp to run step response, input s to stop: ')
-#     Kp_motor1.put(x1)
-#     
-#     ## A variable that requests for set point from the user.
-#     setpt_motor1 = task_share.Share ('l', thread_protect = False, name = "Set Point Share")
-#     y1 = input('Input set point: ')
-#     setpt_motor1.put(y1)
-    
-#     controller.set_gain(Kp_motor1.get())
-#     controller.set_setpoint(setpt_motor1.get())
-#     encoder_drv1.zero()
+    #motor_drv2.enable()
     
     ## A variable that marks the start of the timer.
-    start_time = task_share.Share ('l', thread_protect = False, name = "Start Time Share")
+    #difference = task_share.Share ('l', thread_protect = False, name = "Time Difference Share")
     ## A variable that creates an empty list to be populated with time data.
 #     time1 = task_share.Queue ('l', 16, thread_protect = False, overwrite = False,
 #                            name = "Time Q 1")
@@ -149,10 +106,27 @@ if __name__ == "__main__":
 
     # Run the scheduler with the chosen scheduling algorithm. Quit if any 
     # character is received through the serial port
-    vcp = pyb.USB_VCP ()
-    start_time.put(utime.ticks_ms())
-    while not vcp.any ():
-        cotask.task_list.pri_sched ()
+#     vcp = pyb.USB_VCP ()
+#     while not vcp.any()
+#     start_time.put(utime.ticks_ms())
+    x = input('Input Kp to run step response, input s to stop: ')
+    try:
+        float(x)
+    except:
+        if x == 's':
+            motor_drv1.set_duty_cycle(0)
+    else:
+        ## A variable that requests for set point from the user.
+        y = input('Input set point: ')
+        controller.set_gain(x)
+        controller.set_setpoint(y)
+        encoder_drv1.zero()
+        # Run the scheduler with the chosen scheduling algorithm. Quit if any 
+        # character is received through the serial port
+        vcp = pyb.USB_VCP ()
+        vcp.read()
+        while not vcp.any():
+            cotask.task_list.pri_sched ()
 
     # Empty the comm port buffer of the character(s) just pressed
     vcp.read ()
